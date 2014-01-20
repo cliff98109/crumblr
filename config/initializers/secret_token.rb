6f51824c60592014a0c8e5883c1a4f40667f19ad
cliff98109 <clifffreeman.jobsearch@gmail.com>
2014-01-20 10:02:32 -0800

    first commit
    first skillshare ruby on rails project (crumblr) commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crumblr::Application.config.secret_key_base = '4894383329c666b34aa21ee6c25fcb87a654d66a8f53525bc87d5fe84f13a69bfd449c892fa83156fbba02d0986670c93d43cafe47748b320f7eec09463fadf7'
